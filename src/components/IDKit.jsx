ac6e399aea3154167e73404e278215b1e296d773
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2024-11-04 11:34:03 +0100

    Update IDKit.jsx

--- src/components/IDKit.jsx
+++ src/components/IDKit.jsx
@@ -42,7 +42,7 @@ const IDKit = () => {
         <>
             {!verified ? (
                 <IDKitWidget
-                    app_id="app_7c54dd9d0c581377f255db271d4773b6" // obtained from the Developer Portal
+                    app_id="app_22f503b7107497ff51011caa16433fd2" // obtained from the Developer Portal
                     action="auth" // this is your action name from the Developer Portal
                     false
                     verification_level="device"
